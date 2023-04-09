438c78677fa1e8b8c90eb95992d24aef475c150d
Al Hasanul Banna <tutul5800@gmail.com>
2023-04-09 15:43:21 +0600

    about us page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,45 @@
 import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
-    <div>
-      <h2>This is about page</h2>
+    <div className="about-us-text">
+      <h2>Our Journey</h2>
+      <p>
+        Welcome to our online bookstore! We are a team of passionate book lovers
+        who have come together to create a platform that celebrates the written
+        word and brings together readers and writers from all over the world.
+      </p>
+      <p>
+        Our mission is to provide you with a wide range of books that cover
+        every genre, topic, and interest. Whether you're looking for the latest
+        bestsellers or rare and hard-to-find titles, we've got you covered. Our
+        team works tirelessly to source the best books from all over the world,
+        ensuring that you have access to the latest releases and timeless
+        classics.
+      </p>
+      <p>
+        We believe that books have the power to inspire, educate, and entertain.
+        That's why we are committed to providing a platform that encourages
+        reading and promotes literacy. Our team is made up of experienced book
+        lovers who are passionate about sharing their love for books with
+        others. We believe that everyone should have access to books and the
+        knowledge they contain, regardless of their background or financial
+        situation.
+      </p>
+      <p>
+        We take pride in our commitment to quality and customer service. We
+        understand that buying books online can be a daunting experience, which
+        is why we have created a platform that is easy to use, secure, and
+        reliable. Our team is always on hand to answer any questions you may
+        have, and we are committed to ensuring that you are completely satisfied
+        with your purchase.
+      </p>
+      <p>
+        Thank you for choosing our online bookstore. We hope that you enjoy
+        browsing our selection of books and that you find the perfect title to
+        add to your collection. Happy reading!
+      </p>
     </div>
   );
 };
